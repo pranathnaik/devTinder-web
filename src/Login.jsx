0144ca245e03c547316c64864948543f37eb4717
Pranath Naik <pranathnaik12@gmail.com>
2025-05-24 22:33:55 +0530

    Login module

---
+++ src/Login.jsx
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { useState } from "react";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        {
+          withCredentials: true, // Ensure cookies are sent with the request
+        }
+      );
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
+  return (
+    <div className="flex items-center justify-center my-10">
+      <div className="card card-border bg-base-300 w-96">
+        <div className="card-body">
+          <h2 className="card-title mb-4">Login</h2>
+          <div className="form-control w-full max-w-xs mb-4">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+              placeholder="Enter your email"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs mb-4">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs mb-4">
+            <button
+              className="btn btn-primary w-full"
+              onClick={handleLogin}
+              disabled={!emailId || !password}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
